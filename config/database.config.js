bceb4a9090aa87489fcc4d90ea7f773e7bebf199
Mohit <mohitshah7777@gmail.com>
2021-07-01 12:42:26 +0530

    Welcome to Address Book App

---
+++ config/database.config.js
@@ -0,0 +1,18 @@
+/**
+ * @file         database.config.js
+ * @description  Connection details for database 
+ * @author       Mohit Shah <mohitshah7777@gmail.com>
+ * @since        01/07/2021  
+-----------------------------------------------------------------------------------------------*/
+
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true,  useFindAndModify: false })
+    .then(() => { console.log("Successfully connected to the database"); })
+    .catch(err => { console.log('Could not connect to the database. Exiting now...', err);
+                    process.exit();
+    });
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require('./config/database.config');
+require('dotenv')
+
+// Create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+const port = process.env.PORT
+
+// define a simple route    
+app.get('/',(req,res) => {
+    res.json({"message": "Welcome to Address Book App"})
+})
+
+// listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port : ${port}`);
+});
+
+module.exports = app;
